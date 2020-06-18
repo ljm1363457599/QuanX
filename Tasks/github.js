af0d03b82f2f723044e9d3972b0e77c4923a7e26
ljm1363457599 <57308245+ljm1363457599@users.noreply.github.com>
2020-06-18 10:40:11 +0800

    Update github.js

--- Tasks/github.js
+++ Tasks/github.js
@@ -48,6 +48,11 @@ const repositories = [
     name: "zZPiglet大佬",
     url: "https://github.com/zZPiglet/Task"
   },
+ {
+    name: "yichahucha -- 微博广告",
+    file_names: ["wb_ad.js", "wb_launch.js"],
+    url: "https://github.com/yichahucha/surge/tree/master",//路径模板🌟
+  },
   {
     name: "NobyDa",
     file_names: ["JD-DailyBonus/JD_DailyBonus.js", "52pojie-DailyBonus"],//路径模板🌟
