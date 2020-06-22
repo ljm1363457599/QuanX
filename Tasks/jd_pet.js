18850fe5bc0f5acfae6ea561a3680d3fc841d007
ljm1363457599 <57308245+ljm1363457599@users.noreply.github.com>
2020-06-22 12:42:03 +0800

    Update jd_pet.js

--- Tasks/jd_pet.js
+++ Tasks/jd_pet.js
@@ -91,10 +91,10 @@ const JD_API_HOST = 'https://api.m.jd.com/client.action';
 const cookie = $hammer.read('CookieJD');
 
 var shareCodes = [ // 这个列表填入你要助力的好友的shareCode, 最多可能是5个? 没有验证过
-    'MTAxODcxOTI2NTAwMDAwMDAwMDc4MDExNw==',
+    'MTAxODc2NTE0NzAwMDAwMDAyODE4MTM2OQ==',
     'MTAxODcxOTI2NTAwMDAwMDAyNjA4ODQyMQ==',
-    'MTAxODc2NTEzMDAwMDAwMDAwNTUwNDUxMw==',
-    'MTAxODc2NTEzOTAwMDAwMDAxODQ5MDg5NQ==',
+    'MTAxODc2NTEzMjAwMDAwMDAyNzA4MjkwNw==',
+    'MTAxODc2NTEzMTAwMDAwMDAyNjg1MTg5MQ==',
 ]
 
 var petInfo = null;
